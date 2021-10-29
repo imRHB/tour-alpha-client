23b9b0f3c5c1125e75d7dc1b6910c4d724785d2e
imprantu <rakinhasanbabu4@gmail.com>
2021-10-29 13:48:13 +0600

    project structure created

---
+++ src/Pages/Landing/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className="container">
+            <h1 className="text-center fs-1 fw-bold text-secondary">Welcome to Tour Alpha</h1>
+            <p>This project will be made with React JS, React Bootstrap, React Hook Form, React Router Dom, FontAwesome, Node JS, MongoDB</p>
+        </div>
+    );
+};
+
+export default Home;
