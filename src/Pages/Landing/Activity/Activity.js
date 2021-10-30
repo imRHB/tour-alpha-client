9f62e7239a3e42390a758f832c3a85cccd33dccf
imprantu <rakinhasanbabu4@gmail.com>
2021-10-30 13:57:52 +0600

    dummy text removed from about

---
+++ src/Pages/Landing/Activity/Activity.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Col, Row } from 'react-bootstrap';
+import './Activity.css';
+import act1 from '../../../images/activity/adventure.png'
+import act2 from '../../../images/activity/trekking.png'
+import act3 from '../../../images/activity/camp-fire.png'
+import act4 from '../../../images/activity/off-road.png'
+import act5 from '../../../images/activity/camping.png'
+import act6 from '../../../images/activity/exploring.png'
+
+const Activity = () => {
+    return (
+        <div className="container -my-5">
+            <h2 className="text-center fs-1 fw-bold text-secondary my-5 text-uppercase">Adventure and Activity</h2>
+            <Row xs={2} sm={2} md={3} lg={6} xl={6}>
+                <Col className="h-100">
+                    <div className="text-center p-3 rounded-3 activity-item">
+                        <img src={act1} alt="" />
+                        <p className="fs-5 fw-bold mt-2">Adventure</p>
+                    </div>
+                </Col>
+                <Col className="h-100">
+                    <div className="text-center p-3 rounded-3 activity-item">
+                        <img src={act2} alt="" />
+                        <p className="fs-5 fw-bold mt-2">Adventure</p>
+                    </div>
+                </Col>
+                <Col className="h-100">
+                    <div className="text-center p-3 rounded-3 activity-item">
+                        <img src={act3} alt="" />
+                        <p className="fs-5 fw-bold mt-2">Adventure</p>
+                    </div>
+                </Col>
+                <Col className="h-100">
+                    <div className="text-center p-3 rounded-3 activity-item">
+                        <img src={act4} alt="" />
+                        <p className="fs-5 fw-bold mt-2">Adventure</p>
+                    </div>
+                </Col>
+                <Col className="h-100">
+                    <div className="text-center p-3 rounded-3 activity-item">
+                        <img src={act5} alt="" />
+                        <p className="fs-5 fw-bold mt-2">Adventure</p>
+                    </div>
+                </Col>
+                <Col className="h-100">
+                    <div className="text-center p-3 rounded-3 activity-item">
+                        <img src={act6} alt="" />
+                        <p className="fs-5 fw-bold mt-2">Adventure</p>
+                    </div>
+                </Col>
+            </Row>
+        </div>
+    );
+};
+
+export default Activity;
--- src/Pages/Landing/Home/Home.js
+++ src/Pages/Landing/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import TourPackage from '../../TourAlphaPackage/TourPackage/TourPackage';
+import Activity from '../Activity/Activity';
 import Banner from '../Banner/Banner';
 import DiscountOffer from '../DiscountOffer/DiscountOffer';
 import TeamMember from '../Team/TeamMember/TeamMember';
@@ -11,6 +12,7 @@ const Home = () => {
             <TourPackage></TourPackage>
             <DiscountOffer></DiscountOffer>
             <TeamMember></TeamMember>
+            <Activity></Activity>
         </div>
     );
 };
