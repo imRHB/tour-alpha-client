21765772e714c30568a6916c589eec41b973662a
imprantu <rakinhasanbabu4@gmail.com>
2021-10-31 21:34:32 +0600

    responsiveness fixed

--- src/Pages/Details/DiscountPackDetails/DiscountPackDetails.js
+++ src/Pages/Details/DiscountPackDetails/DiscountPackDetails.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Col, Row } from 'react-bootstrap';
 import { useParams } from 'react-router';
 import { Link } from 'react-router-dom';
 
@@ -17,18 +18,22 @@ const DiscountPackDetails = () => {
     return (
         <div className="container my-5">
             <h2 className="text-center fs-1 fw-bold text-secondary my-5">Package Details</h2>
-            <div className="row">
-                <h1 className="fs-2 fw-bold text-info text-uppercase">{title}</h1>
-                <div className="col-8 my-3 rounded-3">
-                    <img className="img-fluid rounded-3" src={img} alt="" />
-                    <div className="my-4">
-                        <p>{description}</p>
-                        <p>{description}</p>
-                        <p>{description}</p>
-                    </div>
-                </div>
 
-                <div className="col-4 my-3">
+            <Row xs={1} md={1} xl={2} className="g-4">
+                <h1 className="fs-2 fw-bold text-info text-uppercase">{title}</h1>
+
+                <Col className="col-md-12 col-lg-8 col-xl-8 ">
+                    <div className="rounded-3">
+                        <img className="img-fluid rounded-3" src={img} alt="" />
+                        <div className="my-4">
+                            <p>{description}</p>
+                            <p>{description}</p>
+                            <p>{description}</p>
+                        </div>
+                    </div>
+                </Col>
+
+                <Col className="col-md-12 col-lg-4 col-xl-4">
                     <div className="bg-dark text-light p-4 rounded-3">
                         <p className="fs-4 fw-bold">{location}</p>
                         <h2 className="text-danger"><small>Previously</small> <strike>${price}</strike></h2>
@@ -46,8 +51,6 @@ const DiscountPackDetails = () => {
                             <li>Don't draw attention</li>
                             <li>Make copies of important documents</li>
                             <li>Keep your friends and family updated</li>
-                            <li>Be wary of public Wi-Fi</li>
-                            <li>Safeguard your hotel room</li>
                             <li>Be aware of your surroundings</li>
                         </ul>
                     </div>
@@ -59,10 +62,17 @@ const DiscountPackDetails = () => {
                             <li className="list-group-item"><span className="fw-bold me-2">D3</span>Excursion in the natural oasis and picnic</li>
                             <li className="list-group-item"><span className="fw-bold me-2">D4</span>Transfer to the airport and return to the agency</li>
                             <li className="list-group-item"><span className="fw-bold me-2">D5</span>Excursion in the natural oasis and picnic</li>
-                            <li className="list-group-item"><span className="fw-bold me-2">D6</span>Visit the main museums and lunch included</li>
-                            <li className="list-group-item"><span className="fw-bold me-2">D7</span>Departure at the airport and arrival at the hotel</li>
                         </ul>
                     </div>
+                </Col>
+            </Row>
+
+            <div className="row">
+
+
+
+                <div className="col-4 my-3">
+
                 </div>
             </div>
         </div>
