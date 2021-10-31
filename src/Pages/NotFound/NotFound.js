31c03b7abb50225547a9e13b87b229b5ee1cc23f
imprantu <rakinhasanbabu4@gmail.com>
2021-10-31 18:57:04 +0600

    bug fixed

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './NotFound.css';
 
 const NotFound = () => {
     return (
-        <div className="container my-5 text-center">
-            <h1>Error 404</h1>
-            <p>Page not found</p>
-            <NavLink to="/home">Home</NavLink>
+        <div className="container my-5">
+            <div className="d-flex justify-content-center align-items-center">
+                <div className="text-center error-container">
+                    <h1 className="fs-1 fw-bold text-danger">Error 404</h1>
+                    <p className="text-muted">The requested URL was not found on the server</p>
+                    <p>Visit <span className="error-url"><Link to="/home" >Home</Link></span> until the problem is resolved</p>
+                </div>
+            </div>
         </div>
     );
 };
