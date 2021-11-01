0492a32e44553c6ae87baaa62bda0dd46a4e9e52
imprantu <rakinhasanbabu4@gmail.com>
2021-11-01 12:41:35 +0600

    minor bug fixed

--- src/Pages/Landing/Banner/Banner.js
+++ src/Pages/Landing/Banner/Banner.js
@@ -6,8 +6,8 @@ const Banner = () => {
         <div>
             <div className="cover-img" style={{ backgroundImage: `url(${banner})`, backgroundPosition: 'center', backgroundRepeat: 'no-repeat', backgroundAttachment: 'fixed', backgroundSize: 'cover', height: '90vh' }}>
                 {/* background image goes here */}
-                <div className="text-center">
-                    <p className="text-light fs-1 fw-bold pt-5">Enjoy Your Vacation with Tour Alpha</p>
+                <div className="container text-center">
+                    <p className="text-dark fs-1 fw-bold pt-5">To Enjoy Your Vacation Travel with <span className="text-danger">Tour Alpha</span></p>
                 </div>
             </div>
         </div>
